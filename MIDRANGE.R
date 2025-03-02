24bc3aa4529aae3b151ae9f45e56f9baafc88069
Hasrith2004 <139963766+Hasrith2004@users.noreply.github.com>
2025-03-02 10:14:38 +0530

    Create MIDRANGE.R

---
+++ MIDRANGE.R
@@ -0,0 +1,6 @@
+names&lt;-c(&quot;siri&quot;,&quot;mahi&quot;,&quot;chiru&quot;)
+age&lt;-c(23,24,25)
+marks&lt;-c(88,78,25)
+df&lt;-data.frame(names,age,marks)
+mid range(df $age)
+write.csv(df,&quot;datafr.csv&quot;)
