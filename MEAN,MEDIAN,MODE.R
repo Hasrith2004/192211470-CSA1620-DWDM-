ec2f14c0e72f5106d80e32b2c65ea67cc0eb71d5
Hasrith2004 <139963766+Hasrith2004@users.noreply.github.com>
2025-03-02 10:11:14 +0530

    Create MEAN,MEDIAN,MODE.R

---
+++ MEAN,MEDIAN,MODE.R
@@ -0,0 +1,21 @@
+#MEAN
+names&lt;-c(&quot;siri&quot;,&quot;mahi&quot;,&quot;chiru&quot;)
+age&lt;-c(23,24,25)
+marks&lt;-c(88,78,25)
+df&lt;-data.frame(names,age,marks)
+mean(df $age)
+write.csv(df,&quot;datafr.csv&quot;)
+#MODE
+names&lt;-c(&quot;siri&quot;,&quot;mahi&quot;,&quot;chiru&quot;)
+age&lt;-c(23,24,25)
+marks&lt;-c(88,78,25)
+df&lt;-data.frame(names,age,marks)
+mode(df $age)
+write.csv(df,&quot;datafr.csv&quot;)
+#MEDIAN
+names&lt;-c(&quot;siri&quot;,&quot;mahi&quot;,&quot;chiru&quot;)
+age&lt;-c(23,24,25)
+marks&lt;-c(88,78,25)
+df&lt;-data.frame(names,age,marks)
+median(df $age)
+write.csv(df,&quot;datafr.csv&quot;)
